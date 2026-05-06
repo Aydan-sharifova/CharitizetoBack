2c07ed3788e46a2479021cb060610b52a052713e
Aydan Şərifova <aydansrifova@Aydans-MacBook-Air.local>
2026-05-06 12:51:38 +0400

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CharitizeMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
